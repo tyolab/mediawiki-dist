de5c2a6956ff28f811751e61efe0f48ffe70458d
Monfee <eric.tang2005@gmail.com>
2015-07-17 16:52:03 +1000

    Google App Engine config

--- GoogleAppEngineSettings.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$wgGaeAppId = 'xxxpedia3';
-$wgGaeInstanceName = 'xxxpedia';
---
+++ GoogleAppEngineSettingsSample.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Configure your Google App Engine Id
+ * and Google Cloud SQL instance here
+ */
+
+#$wgGaeAppId = '';
+#$wgGaeInstanceName = '';
